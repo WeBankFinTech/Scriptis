0e6eac6970bff8d66f6320aa39f1352a2146dc98
arbor <arbort@163.com>
2019-08-27 17:53:44 +0800

    1.automatic packaging into ZIP packages; 2.add shell scripts when packing

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,18 @@
 // vue.config.js
 let CopyWebpackPlugin = require('copy-webpack-plugin')
+let FileManagerPlugin = require('filemanager-webpack-plugin');
 let path = require('path')
+let fs = require('fs');
+
+const getVersion = () => {
+    const pkgPath = path.join(__dirname, './package.json');
+    let pkg = fs.readFileSync(pkgPath);
+    pkg = JSON.parse(pkg);
+    return pkg.version;
+}
+
 module.exports = {
+    outputDir: 'dist/dist',
     configureWebpack: {
         resolve: {
             alias: {
@@ -17,7 +28,24 @@ module.exports = {
             new CopyWebpackPlugin([{
                 from: 'node_modules/monaco-editor/dev/vs',
                 to: 'static/vs',
+            }, {
+                from: './config.sh',
+                to: '../',
+            }, {
+                from: './install.sh',
+                to: '../',
             }]),
+            // 自动打包成zip包
+            new FileManagerPlugin({
+                onEnd: {
+                    // 先删除根目录下的zip包
+                    delete: [`./wedatasphere-scriptis-${getVersion()}-dist.zip`],
+                    // 将dist文件夹下的文件进行打包
+                    archive: [
+                        { source: './dist', destination: `./wedatasphere-scriptis-${getVersion()}-dist.zip` }
+                    ]
+                },
+            })
         ]
     },
     // 选项...
